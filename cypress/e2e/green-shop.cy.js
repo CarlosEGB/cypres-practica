f5bb6112351b6264d469b465542b7d7dc9efa178
Carlos Garcia <carlos_9655@hotmail.com>
2022-09-17 22:00:37 -0500

    se crea proyecto base para pruebas con cypress

---
+++ cypress/e2e/green-shop.cy.js
@@ -0,0 +1,11 @@
+const cypress = require("cypress");
+
+describe("add prodcuts", () => {
+  
+  it("Open Browser on ", () => {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+  });
+
+  
+
+});
